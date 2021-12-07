20fc0d19d8ba530826d4fc24819439118a62cd57
Khai Pham <pgkhai.dev@gmail.com>
2021-12-07 10:25:09 +0700

    Update Welcome page

--- lib/modules/welcome/pages/welcome_page.dart
+++ lib/modules/welcome/pages/welcome_page.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner/blocs/app_state_bloc.dart';
+import 'package:qr_scanner/providers/bloc_provider.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  AppStateBloc? get appStateBloc => BlocProvider.of<AppStateBloc>(context);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome'),
-      ),
-      body: Center(
-        child: IconButton(
-          icon: const Icon(Icons.arrow_forward),
-          onPressed: () {},
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Welcome to\nQR Scanner App',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline2,
+          ),
+          IconButton(
+            highlightColor: Colors.blue,
+            icon: const Icon(Icons.arrow_forward),
+            onPressed: _changeAppState,
+          ),
+        ],
       ),
     );
+  }
+
+  void _changeAppState() {
+    appStateBloc!.changeAppState(AppState.authorized);
   }
 }
